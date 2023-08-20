2acdf301613a2050bbff0943a6cc3e1f76df8747
fhwang98 <fhwang9815@gmail.com>
2023-08-21 08:57:32 +0900

    강의실 더미 수정

--- src/com/project/room/MakeRoomDummy.java
+++ src/com/project/room/MakeRoomDummy.java
@@ -38,21 +38,30 @@ public class MakeRoomDummy {
 		String[] days = {"월", "화", "수", "목", "금"};
 
 		Random rnd = new Random();
-		String[] courses = new String[50];
-		for (int i = 0; i < 50; i++) {
+		String[] courses = new String[20];
+		for (int i = 0; i < 19; i++) {
 			courses[i] = String.format("ABC%03d", i+1);
 		}
+		courses[19] ="empty";
 		
 		for (int i = 0; i < 20; i++) { //강의실 루프
 			
-			for (int j = 0; j < rnd.nextInt(10); j++) { //스케줄 루프
+			String name = String.format("%d호", 101 + 100*(i/5) + i%5); 
+			String courseCode = courses[rnd.nextInt(20)];
+			//여기서 강좌가 없음이면 다음 강의실로 넘어감
+			if (courseCode.equals("empty")) {
 				
-				//String no = String.format("%03d", i + 1);
-				String name = String.format("%d호", 101 + 100*(i/5) + i%5); 
+				Room r = new Room(i + 1, name, "empty", "empty", courseCode); 
+				list.add(r);
+				continue;
+			}
+			
+			for (int j = 0; j < rnd.nextInt(10)+1; j++) { //스케줄 루프 빈강의실 걸렀으니까 무조건 1개 이상
+				
 				String dayOfWeek = days[rnd.nextInt(5)];
 				String time = String.format("%02d:00", rnd.nextInt(15) + 6);
-				String courseCode = courses[rnd.nextInt(50)];
 				String schedule = i + dayOfWeek + time + courseCode;
+				
 				
 				if (isDuplicate(schedule, list)) {
 					
