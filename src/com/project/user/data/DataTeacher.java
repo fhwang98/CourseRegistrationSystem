56430367aaf815d650898c6cee31db19033e67ce
javaMinjeong <mmjjkk01234@naver.com>
2023-08-20 20:54:17 +0900

    user.data클래스 파일추가

---
+++ src/com/project/user/data/DataTeacher.java
@@ -0,0 +1,100 @@
+package com.project.user.data;
+
+import java.util.Arrays;
+
+public class DataTeacher {
+	private String teacherCode; 
+	private String id; 
+	private String password; 
+	private String name; 
+	private String tel; 
+	private String birth;
+	private int using;
+	
+	public DataTeacher() {
+		this.teacherCode = "";
+		this.id = "";
+		this.password = "";
+		this.name = "";
+		this.tel = "";
+		this.birth = "";
+		this.using = 0;
+	}	
+	
+	public void setDataTeacher(String[] teacherLine) {
+		this.teacherCode = teacherLine[0];
+		this.id = teacherLine[1];
+		this.password = teacherLine[2];
+		this.name = teacherLine[3];
+		this.tel = teacherLine[4];
+		this.birth = teacherLine[5];
+		this.using = Integer.parseInt(teacherLine[6]);
+	}
+
+	public String getTeacherCode() {
+		return teacherCode;
+	}
+
+	public void setTeacherCode(String teacherCode) {
+		this.teacherCode = teacherCode;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
+	public int getUsing() {
+		return using;
+	}
+
+	public void setUsing(int using) {
+		this.using = using;
+	}
+
+	@Override
+	public String toString() {
+		return teacherCode + "," + id + "," + password + "," + name
+				+ "," + tel + "," + birth + ","	+ using;
+	}	
+	
+	
+	
+	
+		
+}
