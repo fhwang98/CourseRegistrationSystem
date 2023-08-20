d95d903e103e16b7d36422934cc773ece63c22f6
fhwang98 <fhwang9815@gmail.com>
2023-08-21 01:01:35 +0900

    Modified AdminMain

--- src/com/project/admin/AdminMain.java
+++ src/com/project/admin/AdminMain.java
@@ -1,14 +1,60 @@
 package com.project.admin;
 
+import java.util.Scanner;
+
 import com.project.admin.notice.NoticeMain;
 import com.project.notice.NoticeData;
 
 public class AdminMain {
+	
+	public static boolean loop;
+	
+	static {
+		loop = true;
+	}
 
 	public static void controlAdmin() {
-		NoticeData.load();
-		NoticeMain.controlNotice();
-
+		
+		Scanner scan = new Scanner(System.in);
+		
+		
+		while (loop) {
+			AdminView.printAdminMain();
+			String sel = scan.nextLine();
+			if (sel.equals("")) {
+				continue;
+			} else if ( !(AdminUtil.isDigit(sel))
+					|| Integer.parseInt(sel) < 1
+					|| Integer.parseInt(sel) > 7) {
+				System.out.println("invalid input");
+				System.out.println("if you wnt keep going enter a new line");
+				scan.nextLine();
+				continue;
+			}else if (sel.equals("1")) {
+				//일반 회원 관리
+				System.out.println("일반 회원 관리");
+			} else if (sel.equals("2")) {
+				//강사 회원 관리
+				System.out.println("강사 회원 관리");
+			} else if (sel.equals("3")) {
+				//강좌 관리
+				System.out.println("강좌 관리");
+			} else if (sel.equals("4")) {
+				//강의실 관리
+				System.out.println("강의실 관리");
+			} else if (sel.equals("5")) {
+				//5. 공지사항
+				NoticeData.load();
+				NoticeMain.controlNoticeMain();
+			} else if (sel.equals("6")) {
+				//내정보
+			}  else if (sel.equals("7")) {
+				//로그아웃
+				System.out.println("log out");
+				loop = false;
+			}
+		}
+		
 	}
 	
 }
