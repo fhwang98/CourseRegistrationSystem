fe2c7a53cc8734bb15b7e5d1c3947aca85604b6a
javaMinjeong <mmjjkk01234@naver.com>
2023-08-21 11:37:20 +0900

    주석 추가중

--- src/com/project/user/login/LoginTeacher.java
+++ src/com/project/user/login/LoginTeacher.java
@@ -45,7 +45,7 @@ public class LoginTeacher {
 			
 			System.out.println("로그인이 완료되었습니다.");
 			return UserDbms.searchTeacherByIdPw(id, password);
-			
+			//로그인 후 화면으로 이동해야함
 		}else {
 			System.out.println();
 			System.out.println("======================");
