1e184537e10f8a9918fe1f7f87bd0f0196fdc2f7
suding <>
2023-08-21 14:00:41 +0900

    수강신청 날짜 캘린더 이용

--- src/com/project/course/CourseApplication.java
+++ src/com/project/course/CourseApplication.java
@@ -3,7 +3,7 @@ package com.project.course;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class CourseApplication {
@@ -39,71 +39,103 @@ public class CourseApplication {
 //			Main.mainMent();
 		}
 
+//전체 수강 내역에 추가
+//전체 수강 내역에서 회원 코드가 같을 경우 해당 강좌 코드를 확인하고 해당 강좌의 시간을 비교하여 같을경우 수강 시간 겹친다고 멘트출력
+		// 일반회원코드 > temp[0]
+		// 강사회원코드 > temp[0]
+
 		try {
 
 			BufferedReader reader = new BufferedReader(new FileReader("data/lectureList.txt"));
 
 			String line = null;
 
-			
 			boolean courseFound = false;
-			
+
 			while ((line = reader.readLine()) != null) {
 
 				String[] temp = line.split(",");
 
-				if (temp[0].equalsIgnoreCase(code.toUpperCase())) {
-					Course c = new Course(temp[0], temp[1], temp[2], temp[3], temp[4], temp[5], temp[6], temp[7],
-							temp[8], temp[9], temp[10], temp[11]);
-					
+				Course c = new Course(temp[0], temp[1], temp[2], temp[3], temp[4], temp[5], temp[6], temp[7], temp[8],
+						temp[9], temp[10], temp[11]);
+
+				if (temp[0].equalsIgnoreCase(code)) {
+
 					courseMent(c);
 
 					String input = scan.nextLine();
 
-					if (input.equals("y")) {
-						int tempValue = Integer.parseInt(temp[10]);
+					if (courseFound = true) {
 						
-						if (tempValue < 202308) {
+						if (input.equals("y")) {
+						// 여기부터
+
+							String tempValue = temp[10];
+
+							int year = Integer.parseInt(tempValue.substring(0, 4));
+							int month = Integer.parseInt(tempValue.substring(4, 6));
+
+							Calendar nowTime = Calendar.getInstance();
+							Calendar courseTime = Calendar.getInstance();
+
+							courseTime.set(year, month - 1, 1);
+
+							long courseTimeTick = courseTime.getTimeInMillis();
+							long nowTimeTick = nowTime.getTimeInMillis();
+
+							long gap = nowTimeTick - courseTimeTick;
+
+							// 여기까지
+
+							if (gap > 0) {
+								System.out.println();
+								System.out.println("\"이미 신청이 마감된 강좌 입니다.\"");
+								System.out.println("\"처음으로 이동합니다.\"");
+								System.out.println();
+
+							//	Main.mainMent();
+								break;
+
+							} else {
+								System.out.println();
+								System.out.println("\"수강신청이 완료되었습니다.\"");
+								System.out.println();
+							//	Main.mainMent();
+								break;
+							}
+
+						} else if (input.equals("n")) {
 							System.out.println();
-							System.out.println("\"이미 신청이 마감된 강좌 입니다.\"");
 							System.out.println("\"처음으로 이동합니다.\"");
 							System.out.println();
-//							Main.mainMent();
-						} else {
-							System.out.println();
-							System.out.println("\"수강신청이 완료되었습니다.\"");
-							System.out.println();
-							
-//							Main.mainMent();
+						//	Main.mainMent();
+							break;
 						}
-					} else if(input.equals("n")) {
-						System.out.println();
-						System.out.println("\"처음으로 이동합니다.\"");
-						System.out.println();
-//						Main.mainMent();
-						courseFound = true;
-						break;
 					}
 
-				} 
-				
-				if(!courseFound){
-					System.out.println();
-					System.out.println("\"잘못된 강좌코드 입니다.\"");
-					System.out.println("\"강좌코드를 확인해 주세요.\"");
-					System.out.println();
-//					Main.mainMent();
 				}
-
+			
+			} if (!courseFound) {
+				System.out.println();
+				System.out.println("\"잘못된 강좌코드 입니다.\"");
+				System.out.println("\"강좌코드를 확인해 주세요.\"");
+				System.out.println();
+			//	Main.mainMent();
+			
 			}
+
 			reader.close();
 
-			} catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 
 		}
 
 	}
+
+	// 모든 회원 전체 수강내역에 추가
+
+	// 개인 별 수강내역 추가
 
 	private static void courseMent(Course c) {
 		if (c != null) {
