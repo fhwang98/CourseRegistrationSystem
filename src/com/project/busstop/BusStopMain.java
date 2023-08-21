743a61792ae482112203fb3bc0611e92fe7c3c88
eeunha <eunha.dev@gmail.com>
2023-08-21 12:01:40 +0900

    셔틀버스/주석 추가

--- src/com/project/busstop/BusStopMain.java
+++ src/com/project/busstop/BusStopMain.java
@@ -3,8 +3,8 @@ package com.project.busstop;
 import java.util.Scanner;
 
 public class BusStopMain {
-	public static boolean outLoop = true;
-	public static boolean inLoop = true;
+	public static boolean outLoop = true; // 바깥 반복문 제어
+	public static boolean inLoop = true; // 안쪽 반복문 제어
 
 	public static void main(String[] args) {
 		
