cd7ac47dd457a3fcc01ee540f84d327849bfdf24
fhwang98 <fhwang9815@gmail.com>
2023-08-21 11:29:27 +0900

    RoomView, RoomMain 클래스 추가

---
+++ src/com/project/admin/room/RoomView.java
@@ -0,0 +1,16 @@
+package com.project.admin.room;
+
+public class RoomView {
+
+	public static void printRoomMenu() {
+		System.out.println("=====================================");
+		System.out.println("\t\t\t\t강의실 관리");
+		System.out.println("=====================================");
+		System.out.println("\t0.\t뒤로가기");
+		System.out.println("\t1.\t전체 강의실 조회");
+		System.out.println("\t2.\t강의실 검색");
+		System.out.println("--------------------------------------------------");
+		System.out.println("번호 입력:");
+	}
+	
+}
