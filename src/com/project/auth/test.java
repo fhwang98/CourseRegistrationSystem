a890d24c70759926ecf3ec49f23d68388cc21c54
javaMinjeong <mmjjkk01234@naver.com>
2023-08-23 11:04:05 +0900

    테스트

--- src/com/project/auth/test.java
+++ src/com/project/auth/test.java
@@ -6,17 +6,17 @@ import com.project.user.signup.SignUpMain;
 
 public class test {
 
-//	public static void main(String[] args) {
-//		LoginMain main = new LoginMain();
-//		main.LoginProcess();
+	public static void main(String[] args) {
+		LoginMain main = new LoginMain();
+		main.LoginProcess();
 		
 //		public static void main(String[] args) {
 //			SignUpMain main = new SignUpMain();
 //			main.SignUpProcess() ;
-//		
-		public static void main(String[] args) {
-			
-			UserDbms reLoadMember = new UserDbms();
+		
+//		public static void main(String[] args) {
+//			
+//			UserDbms reLoadMember = new UserDbms();
 		}
 
 	}
