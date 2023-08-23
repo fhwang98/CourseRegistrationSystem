4aec5e04f14f3c44ea4b5a97242c234132354cea
javaMinjeong <mmjjkk01234@naver.com>
2023-08-23 10:01:01 +0900

    생성자 테스트

--- src/com/project/auth/test.java
+++ src/com/project/auth/test.java
@@ -1,19 +1,24 @@
 package com.project.auth;
 
+import com.project.user.data.UserDbms;
 import com.project.user.login.LoginMain;
 import com.project.user.signup.SignUpMain;
 
 public class test {
 
-	public static void main(String[] args) {
-		LoginMain main = new LoginMain();
-		main.LoginProcess();
+//	public static void main(String[] args) {
+//		LoginMain main = new LoginMain();
+//		main.LoginProcess();
 		
 //		public static void main(String[] args) {
 //			SignUpMain main = new SignUpMain();
 //			main.SignUpProcess() ;
 //		
+		public static void main(String[] args) {
+			
+			UserDbms reLoadMember = new UserDbms();
+		}
 
 	}
 
-}
+
