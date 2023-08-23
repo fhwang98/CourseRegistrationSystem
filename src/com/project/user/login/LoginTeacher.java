27c5fa790e740fc32f2e5ac1aae853b2d681ecc0
eeunha <eunha.dev@gmail.com>
2023-08-24 01:56:58 +0900

    로그인유저정보 얻는 Authentication 객체 생성

--- src/com/project/user/login/LoginTeacher.java
+++ src/com/project/user/login/LoginTeacher.java
@@ -2,6 +2,7 @@ package com.project.user.login;
 
 import java.util.Scanner;
 
+import com.project.authentication.Authentication;
 import com.project.teacher.TeacherMain;
 import com.project.user.data.DataTeacher;
 import com.project.user.data.UserDbms;
@@ -16,53 +17,57 @@ public class LoginTeacher {
 		System.out.println("    강사 회원 로그인      ");
 		System.out.println("======================");
 		System.out.println();
-		
-		int PwErrorCnt =  0;
-		
-		while(true) {
+
+		int PwErrorCnt = 0;
+
+		while (true) {
 			System.out.print("아이디: ");
 			id = scan.nextLine();
 			System.out.println();
 			System.out.print("비밀번호: ");
 			password = scan.nextLine();
-			//아이디 틀린경우 
-			if(UserDbms.searchTeacherById(id) == null) {
+			// 아이디 틀린경우
+			if (UserDbms.searchTeacherById(id) == null) {
 				System.out.println("아이디 또는 비밀번호가 틀립니다.");
-			}else if(UserDbms.searchTeacherByIdPw(id, password) == null) {
+			} else if (UserDbms.searchTeacherByIdPw(id, password) == null) {
 				System.out.println("비밀번호가 틀립니다.");
 				PwErrorCnt++;
 				System.out.println();
-			}else {
+			} else {
 				break;
 			}
-			if(PwErrorCnt >=5) {
+			if (PwErrorCnt >= 5) {
 				FindData.resetPw();
 				break;
 			}
 		}
-		
-		if(!(PwErrorCnt >=5)) {
+
+		if (!(PwErrorCnt >= 5)) {
 			System.out.println();
 			System.out.println("======================");
 			System.out.println();
-			
+
+			//세 줄 추가
+			DataTeacher curTeacher = UserDbms.searchTeacherByIdPw(id, password);
+			Authentication.loginUserCode = curTeacher.getTeacherCode();
+			System.out.println(Authentication.loginUserCode);
+
 			System.out.println("로그인이 완료되었습니다.");
-			//TODO 로그인 후 화면으로 이동해야함
-			TeacherMain.LoginTeacher();
-			
+			// TODO 로그인 후 화면으로 이동해야함
+			TeacherMain.LoginTeacher(null); //null 추가
+
 			return UserDbms.searchTeacherByIdPw(id, password);
-		}else {
+		} else {
 			System.out.println();
 			System.out.println("======================");
 			System.out.println();
-			
+
 			System.out.println("비밀번호 입력 횟수를 초과했습니다.");
 			System.out.println("비밀번호를 재설정 해주세요.");
-			
+
 			return null;
 		}
 
-		
 	}
 
 }
